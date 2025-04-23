9ff9db4d6a841f316256d974ac4aec1a4483812c
Yago Yàsser <yagoyasser2001@gmail.com>
2025-04-24 00:51:47 +0200

    Implementació botons barra superior. Arranjament condicionals text Clic(s). Afegit widgets (botons + separador) peu de pantalla

--- pantalla_comptador/lib/pantalles/comptador.dart
+++ pantalla_comptador/lib/pantalles/comptador.dart
@@ -14,7 +14,25 @@ class _ComptadorState extends State<Comptador> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pantalles: Comptador')
+        title: const Text('Pantalles: Comptador'),
+        leading: IconButton(
+          onPressed: () {
+            setState(() {
+              contadorClics = 0;
+            });
+          },
+          icon: Icon(Icons.refresh_rounded),
+        ), // leading: Es mostra principi (part esquerra). Solament 1 widget
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                contadorClics = 0;
+              });
+            },
+            icon: Icon(Icons.refresh_rounded),
+          ),
+        ], // actions: Es mostra al final (part dreta). Admet un llistat de widgets
       ),
       body: Center(
         child: Column(
@@ -24,25 +42,56 @@ class _ComptadorState extends State<Comptador> {
               contadorClics.toString(),
               style: const TextStyle(
                 fontSize: 160,
-                fontWeight: FontWeight.w100
-              )
+                fontWeight: FontWeight.w100,
+              ),
             ),
-            Text(
-              'Clic${contadorClics == 1 ? '':'s'}',
-              style: const TextStyle(
-                fontSize: 160,
-                fontWeight: FontWeight.w100
+
+            if (contadorClics == 1 || contadorClics == -1)
+              Text(
+                'Clic',
+                style: const TextStyle(
+                  fontSize: 160,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
+
+            if (contadorClics != 1 && contadorClics != -1)
+              Text(
+                'Clics',
+                style: const TextStyle(
+                  fontSize: 160,
+                  fontWeight: FontWeight.w100,
+                ),
               )
-            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          contadorClics++;
-          setState(() {}); // Actualitza el contingut dels widgets modificats
-        },
-        child: const Icon(Icons.plus_one),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(
+                () {
+                  contadorClics++;
+                },
+              );
+            },
+            child: const Icon(Icons.plus_one),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            shape: const StadiumBorder(), // Canvia la forma del botó a una circular
+            onPressed: () {
+              setState(
+                () {
+                  contadorClics--;
+                },
+              ); 
+            },
+            child: const Icon(Icons.exposure_minus_1_outlined),
+          ),
+        ],
       ),
     );
   }
